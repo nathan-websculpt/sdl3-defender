0bf0fa4c35aa0a4f59098bf8f5759987e127e076
nathan-websculpt <websculpt.nathan@gmail.com>
2025-11-04 13:51:08 -0600

    refactoring: globals cleanup

--- core/globals.h
+++ core/globals.h
@@ -16,28 +16,3 @@ public:
 
 // declare the global instance using 'extern'
 extern Globals globals;
-
-
-
-
-
-// #pragma once
-
-// class Globals {
-// public:
-//     Globals() = default;
-//     int getInitialWindowWidth() { return initialWindowWidth; }
-//     int getInitialWindowHeight() { return initialWindowHeight; }
-
-//     int getWindowWidth() { return windowWidth; }
-//     void setWindowWidth(int w) { windowWidth = w; }
-
-//     int getWindowHeight() { return windowHeight; }
-//     void setWindowHeight(int h) { windowHeight = h; }
-    
-// private:
-//     int initialWindowWidth = 800;
-//     int initialWindowHeight = 600;
-//     int windowWidth = 800;
-//     int windowHeight = 600;
-// }
